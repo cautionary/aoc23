c704311bf1d9f01d77ef501773879f87bd098796
cautionary <cautionary@pm.me>
2023-12-09 07:38:22 -0600

    day 9

---
+++ d09p02.py
@@ -0,0 +1,31 @@
+infile = open('d09i01.txt', 'r')
+lines = infile.readlines()
+
+reports =[]
+for line in lines:
+    report = []
+    for num in line.split():
+        report.append(int(num))
+    reports.append(report)
+
+sum = 0
+
+for report in reports:
+    alldifferences = [report]
+    allzeros = False
+    while not allzeros:
+        differences = []
+        for i in range(1, len(alldifferences[-1])):
+            differences.append(alldifferences[-1][i] - alldifferences[-1][i-1])
+        allzeros = True
+        for difference in differences:
+            if difference != 0:
+                allzeros = False
+        alldifferences.append(differences)
+
+    for i in range(len(alldifferences)-1, 0, -1):
+        alldifferences[i-1].insert(0, alldifferences[i-1][0] - alldifferences[i][0])
+
+    sum += alldifferences[0][0]
+
+print(sum)
