8b7393746b1be06e31e6e20836ff6c5708ce0aaa
cautionary <cautionary@pm.me>
2023-12-10 22:55:13 -0600

    finished day 10

--- d10p02.py
+++ d10p02.py
@@ -1,4 +1,4 @@
-infile = open('d10s03.txt', 'r')
+infile = open('d10i01.txt', 'r')
 lines = infile.readlines()
 
 
@@ -54,6 +54,11 @@ while not loopdone:
 
 outsidetiles = set()
 
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if complex(i, j) not in looptiles:
+            groundtiles.add(complex(i, j))
+
 for groundtile in groundtiles.copy():
     crossedtiles = set()
     if groundtile.real < len(lines) / 3:
@@ -97,144 +102,37 @@ for groundtile in groundtiles.copy():
             if remove:
                 outsidetiles.update(crossedtiles)
                 groundtiles.remove(groundtile)
-            
+
+
 for groundtile in groundtiles.copy():
-    allchecked = False
-    visited = set()
-    possible = {groundtile}
-    while not allchecked:
-        curpos = possible.pop()
-        visited.add(curpos)
-        if curpos in outsidetiles:
-            groundtiles.remove(groundtile)
-            allchecked = True
-        if curpos.real.is_integer() and curpos.imag.is_integer():
-            for i, nextpos in enumerate([curpos-1, curpos+1, curpos-1j, curpos+1j]):
-                if nextpos not in visited:
-                    if nextpos in looptiles:
-                        tile = lines[int(nextpos.real)][int(nextpos.imag)]
-                        if i in [0,1]:
-                            if tile == '-':
-                                visited.add(nextpos)
-                            elif tile in ['|', 'J', '7']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag+1)] in ['|', 'F', 'L']:
-                                    if nextpos + 0.5j not in possible and nextpos + 0.5j not in visited:
-                                        possible.add(nextpos + 0.5j)
-                            elif tile in ['|', 'F', 'L']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag-1)] in ['|', 'J', '7']:
-                                    if nextpos - 0.5j not in possible and nextpos - 0.5j not in visited:
-                                        possible.add(nextpos - 0.5j)
-                        elif i in [2,3]:
-                            if tile == '|':
-                                visited.add(nextpos)
-                            elif tile in ['-', 'J', 'L']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real+1)][int(nextpos.imag)] in ['-', '7', 'F']:
-                                    if nextpos + 0.5 not in possible and nextpos + 0.5 not in visited:
-                                        possible.add(nextpos + 0.5)
-                            elif tile in ['-', '7', 'F']:
-                                if lines[int(nextpos.real-1)][int(nextpos.imag)] in ['-', 'J', 'L']:
-                                    if nextpos - 0.5 not in possible and nextpos - 0.5 not in visited:
-                                        possible.add(nextpos - 0.5)
-                    else:
-                        possible.add(nextpos)
+    testline = ""
+    for i in range(int(groundtile.imag)):
+        if complex(groundtile.real, i) in looptiles:
+            testline = testline + lines[int(groundtile.real)][i]
 
-        elif not curpos.imag.is_integer():
-            for i, nextpos in enumerate([curpos - 1 - 0.5j, curpos - 1 + 0.5j, curpos + 1 - 0.5j, curpos +1 + 0.5j]):
-                if nextpos not in visited:
-                    if nextpos in looptiles:
-                        tile = lines[int(nextpos.real)][int(nextpos.imag)]
-                        if i == 0:
-                            if tile == '-':
-                                visited.add(nextpos)
-                            elif tile in ['|', 'J', '7']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag+1)] in ['|', 'F', 'L']:
-                                    if nextpos + 0.5j not in possible and nextpos + 0.5j not in visited:
-                                        possible.add(nextpos + 0.5j)
-                            elif tile in ['|', 'F', 'L']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag-1)] in ['|', 'J', '7']:
-                                    if nextpos - 0.5j not in possible and nextpos - 0.5j not in visited:
-                                        possible.add(nextpos - 0.5j)
-                        elif i == 2:
-                            if tile == '-':
-                                visited.add(nextpos)
-                            elif tile in ['|', 'J', '7']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag+1)] in ['|', 'F', 'L']:
-                                    if nextpos + 0.5j not in possible and nextpos + 0.5j not in visited:
-                                        possible.add(nextpos + 0.5j)
-                            elif tile in ['|', 'F', 'L']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag-1)] in ['|', 'J', '7']:
-                                    if nextpos - 0.5j not in possible and nextpos - 0.5j not in visited:
-                                        possible.add(nextpos - 0.5j)
+    tiles = testline.replace('-', '').replace('FJ', '|').replace('F7', '').replace('LJ', '').replace('L7', '|')
 
-                    else:
-                        possible.add(nextpos)
+    if tiles.count('|') % 2 == 0:
+        groundtiles.remove(groundtile)
+        
 
-        elif not curpos.real.is_integer():
-            for i, nextpos in enumerate([curpos - 0.5 - 1j, curpos - 0.5 + 1j, curpos + 0.5 - 1j, curpos + 0.5 + 1j]):
-                if nextpos not in visited:
-                    if nextpos in looptiles:
-                        tile = lines[int(nextpos.real)][int(nextpos.imag)]
-                        if i == 0:
-                            if tile == '|':
-                                visited.add(nextpos)
-                            elif tile in ['-', 'J', 'L']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag+1)] in ['-', 'F', '7']:
-                                    if nextpos + 0.5 not in possible and nextpos + 0.5 not in visited:
-                                        possible.add(nextpos + 0.5)
-                            elif tile in ['-', 'F', '7']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag-1)] in ['-', 'J', 'L']:
-                                    if nextpos - 0.5 not in possible and nextpos - 0.5 not in visited:
-                                        possible.add(nextpos - 0.5)
-                        elif i == 2:
-                            if tile == '|':
-                                visited.add(nextpos)
-                            elif tile in ['-', 'J', 'L']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag+1)] in ['-', 'F', '7']:
-                                    if nextpos + 0.5 not in possible and nextpos + 0.5 not in visited:
-                                        possible.add(nextpos + 0.5)
-                            elif tile in ['-', 'F', '7']:
-                                visited.add(nextpos)
-                                if lines[int(nextpos.real)][int(nextpos.imag-1)] in ['-', 'J', 'L']:
-                                    if nextpos - 0.5 not in possible and nextpos - 0.5 not in visited:
-                                        possible.add(nextpos - 0.5)
-                    else:
-                        possible.add(nextpos)
+#for i, line in enumerate(lines):
+#    if i < 100:
+#        print(' ', end='')
+#    if i < 10:
+#        print(' ', end='')
+#    print("%d " % i, end='')
+#    for j, c in enumerate(line):
+#        if complex(i, j) in looptiles:
+#            print(c, end = '')
+#        elif complex(i, j) in groundtiles:
+#            print('▩', end='')
+#        elif complex(i, j) in outsidetiles:
+#            print('0', end='')
+#        else:
+#            print(' ', end='')
+#    print()
 
-                
-        print(curpos, possible, visited)
-                
-        if not possible:
-            allchecked = True
-    print()
-
-for i, line in enumerate(lines):
-    if i < 100:
-        print(' ', end='')
-    if i < 10:
-        print(' ', end='')
-    print("%d " % i, end='')
-    for j, c in enumerate(line):
-        if complex(i, j) in looptiles:
-            print(c, end = '')
-        elif complex(i, j) in groundtiles:
-            print('▩', end='')
-        elif complex(i, j) in outsidetiles:
-            print('0', end='')
-        else:
-            print(' ', end='')
-    print()
-
-#46
 
 
 print(len(groundtiles))
